8fedeec03e069d499451a375158b119bd3408d71
LeonitaNika <58749225+LeonitaNika@users.noreply.github.com>
2020-03-27 22:26:16 +0100

    Update beale-cipher.cs

--- beale-cipher.cs
+++ beale-cipher.cs
@@ -1,1 +1,32 @@
+ private static string Enkriptimi(string libri, string plaintext)
+        {
+    
+            libri = File.ReadAllText(@"libri.txt");
+            libri = libri.ToLower();
+            
+            
+            
+            
+            char[] plain = plaintext.ToCharArray();
+            string ch = "";
+            string ch1 = "";
+
+            Random random = new Random();
+
+            for (int i = 0; i < plaintext.Length; i++)
+            {
+                string strCh = "";
+               
+                if (plaintext[i] == ' ')
+                {
+                    continue;
+                }
+                
+                
+                
+                
+                string[] strArray = strCh.Split(' ');
+                int k = random.Next(strArray.Length - 1);
+
+
 
