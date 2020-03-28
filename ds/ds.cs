e0867648bfb0f97755fdc390190556f755a19778
MihrijeK <42340737+MihrijeK@users.noreply.github.com>
2020-03-29 00:12:27 +0100

    Disa modifikime

--- ds/ds.cs
+++ ds/ds.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace ds
 {
@@ -18,45 +19,66 @@ namespace ds
             if (args[0].Equals("Beale"))
             {
 
-                Bealee beale = new Bealee();
+                Beale beale = new Beale();
 
-                if (args[1].Equals("e"))
+                if (args[1].Equals("encrypt"))
                 {
                     string plaintext = args[3];
                     string libri = args[2];
-                    Console.WriteLine(Bealee.Enkriptimi(libri, plaintext));
+                    Console.WriteLine(Beale.Enkriptimi(libri, plaintext));
                 }
 
-else if (args[1].Equals("d"))
+               else if (args[1].Equals("decrypt"))
                 {
                     var text = args[2];
                     string decrypt = args[3];
-                    Bealee.Dekriptimi(decrypt, text);
+                    Beale.Dekriptimi(decrypt, text);
                 }
             }
             if (args[0].Equals("Playfair"))
             {
-                Playfaiir playfair = new Playfaiir();
+                Playfair playfair = new Playfair();
 
-                if (args[1].Equals("e"))
+                if (args[1].Equals("encrypt"))
                 {
                     string plainText = args[3];
-                    Playfaiir.initPlainText(plainText);
+                    Playfair.initPlainText(plainText);
 
                     string qelesi = args[2];
-                    Playfaiir.Enkriptimi(Playfaiir.Krijotabelen(qelesi), Playfaiir.Krijoplaintekstiin());
-                } else if (args[1].Equals("d"))
+                    Playfair.Enkriptimi(Playfair.Krijotabelen(qelesi), Playfair.Krijoplaintekstiin());
+                } else if (args[1].Equals("decrypt"))
                 {
                     string plainText = args[3];
-                    Playfaiir.initPlainText(plainText);
+                    Playfair.initPlainText(plainText);
 
                     string qelesi = args[2];
 
 
 
-                    Playfaiir.Dekriptimi(Playfaiir.Krijotabelen(qelesi), Playfaiir.Krijoplaintekstiin());
+                    Playfair.Dekriptimi(Playfair.Krijotabelen(qelesi), Playfair.Krijoplaintekstiin());
                 }
 
                 Console.ReadKey();
 
             }
+            if (args[0].Equals("morse-code"))
+            {
+                Translator morse = new Translator();
+
+                string input = args[2];
+                string encode_decode = args[1];
+                Code c = new Code(input);
+                // Metoda audio() perkrahet vetem ne Windows
+                if (encode_decode == "audio")
+                {
+                    c.audio(input);
+                }
+                else
+                {
+                    Console.WriteLine("{1}", c.GetInput(), c.Perkthe(encode_decode));
+            
+                }
+            }
+        }
+    }
+}   
