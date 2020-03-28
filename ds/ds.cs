23c74346d38ab0a02cf9a602f90fbd2015952cd8
GresaSalihu <58749187+GresaSalihu@users.noreply.github.com>
2020-03-28 23:56:46 +0100

    Update ds.cs

--- ds/ds.cs
+++ ds/ds.cs
@@ -1,1 +1,30 @@
+using Playfair;
+using ds;
+using Beale;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            if (args[0].Equals("Beale"))
+            {
+
+                Bealee beale = new Bealee();
+
+                if (args[1].Equals("e"))
+                {
+                    string plaintext = args[3];
+                    string libri = args[2];
+                    Console.WriteLine(Bealee.Enkriptimi(libri, plaintext));
+                }
+
 
