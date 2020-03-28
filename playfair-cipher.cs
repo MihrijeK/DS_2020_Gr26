ccaa263996e938108c89b77f0150232e09805577
LeonitaNika <58749225+LeonitaNika@users.noreply.github.com>
2020-03-28 16:57:31 +0100

    Update playfair-cipher.cs

--- playfair-cipher.cs
+++ playfair-cipher.cs
@@ -80,24 +80,29 @@ namespace Playfair
             
             ////nese gjatesia e listes eshte tek ateher shto X 
             //p.sh kemi fjalen siguria ateher vendosim x dhe do fitojm siguriax
-            if (lista1.Count % 2 == 1) lista1.Add('X');
+            if (lista1.Count % 2 == 1) lista1.Add('x');
 
             return lista1;
         }
         static List<char> merrAlfabetin()
         {
             List<char> lista1 = new List<char>();
-            for (char i = 'A'; i <= 'Z'; i++)
-            {
-                if (i == 'J') continue;
+            //marrin alfabetin nga shkronja A ne Z
+            for (char i = 'a'; i <= 'z'; i++)
+            { //nese shkronja eshte j  kaloje
+                if (i == 'j') continue;
+                //shto shkronjen
                 lista1.Add(i);
             }
-            return lista1;
+            return lista1; //kthe listen e fituar
         }
+        //e kemi kriju edhe tabelen per piket shtese
         static List<char> Krijotabelen(string qelesi)
-        {
-            qelesi = qelesi.ToUpper();
+        { //qelesin ktheje ne shkronja te vogla
+            qelesi = qelesi.ToLower();
+             //krijo nje liste
             List<char> lista1 = new List<char>();
+             //krijojm nje liste qe e merr Alfabetin qe e krijuam me par
             List<char> alfabeti = merrAlfabetin();
             lista1 = qelesi.Distinct().ToList(); //Largon shkronjat duplikate nga qelesi
 
@@ -112,31 +117,34 @@ namespace Playfair
                     }
                 }
             }
+             //per qdo vlere ne alfabet
              foreach (var vlera in alfabeti)
-            {
+            {    //shto vleren ne listen1
                 lista1.Add(vlera);
             }
+            //kthe listen e fituar
             return lista1;
         }
+        //krijojm nje funksion per enkriptim qe si parameter dy lista:listen table dhe listen plaintext
         static void Enkriptimi(List<char> table, List<char> plainText)
         {
-            int gjeresia = 5, lartesia = 5;
-            char[,] lista1 = new char[gjeresia, lartesia];//Deklarimi i matrices 5x5
-
+            
+            char[,] lista1 = new char[5, 5];//krijojm  matricen  5x5
             int s = 0;
             //Shendrro nje varg nje dimensional ne varg dydimensional
-            for (int i = 0; i < gjeresia * lartesia; i++)
+            for (int i = 0; i <25; i++)
             {
-
-                lista1[i / gjeresia, i % lartesia] = table[s];
+                // (i/5)-rreshti dhe (i%5)-kolona
+                lista1[i /5, i % 5] = table[s];
                 s++;
             }
 
             //Printon tabelen ne vargje dydimensionale
-            for (int i = 0; i < gjeresia * lartesia; i++)
-            {
+            for (int i = 0; i < 25; i++)
+            {  //nese kemi arritur te kolona e fundit kalo ne rresht te ri
                 if (i % 5 == 0) Console.WriteLine();
-                Console.Write(lista1[i / gjeresia, i % lartesia] + "  ");
+                 //shtyp shkronjat e matrices
+                Console.Write(lista1[i / 5, i % 5] + "  ");
 
             }
             Console.WriteLine();
@@ -156,33 +164,34 @@ namespace Playfair
                 char k2 = plainText[i + 1];//karakteri i dyte
                 int row1 = 0, row2 = -1, col1 = 0, col2 = -1;
 
-                for (int j = 0; j < gjeresia * lartesia; j++)// you can replace this with insted for loop
+                for (int j = 0; j < 25; j++)
                 {
 
-                    if (lista1[j / gjeresia, j % lartesia] == k1) // merr indeksin e karakterit te pare te rreshtit dhe kolones
+                    if (lista1[j / 5, j % 5] == k1) // merr indeksin e karakterit te pare te rreshtit dhe kolones
                     {
-                        row1 = j / gjeresia;
-                        col1 = j % lartesia;
+                        row1 = j / 5;
+                        col1 = j % 5;
                     }
-                    if (lista1[j / gjeresia, j % lartesia] == k2) // merr indeksin e karakterit te dyte te rreshtit dhe kolones
+                    if (lista1[j / 5, j % 5] == k2) // merr indeksin e karakterit te dyte te rreshtit dhe kolones
                     {
-                        row2 = j / gjeresia;
-                        col2 = j % lartesia;
+                        row2 = j / 5;
+                        col2 = j % 5;
                     }
                 }
 
-                //Kontrollo nese karakteri i pare dhe i dyti jane ne te njejtin rreshte
+                //rregulla 1: nese karakteri i pare dhe i dyti jane ne te njejtin rreshte atehere:
                 if (row1 == row2)
-                {
-                    Console.Write(lista1[row1, (col1 + 1) % lartesia] + "" + lista1[row2, (col2 + 1) % lartesia]);
+                {   //merret elementi ne rreshtin e njejt por ne kolonen e ardhshme(nga ana e djatht) te secilit karakter
+                    Console.Write(lista1[row1, (col1 + 1) % 5] + "" + lista1[row2, (col2 + 1) % 5]);
                 }
-                //Kontrollo nese karakteri i pare dhe i dyti jane ne te njejten kolone
+                //Rregulla 2: nese karakteri i pare dhe i dyti jane ne te njejten kolone
                 else if (col1 == col2)
-                {
-                    Console.Write(lista1[(row1 + 1) % lartesia, col1] + "" + lista1[(row2 + 1) % lartesia, col2]);
+                {  //merr elementin qe ndodhet ne rreshtin e ardhshem por ne kolonen e njejte
+                    Console.Write(lista1[(row1 + 1) % 5, col1] + "" + lista1[(row2 + 1) % 5, col2]);
                 }
+                 //Rregulla 3 : nese nuk jane as ne te njejtin rreshtin as ne te njejten kolone
                 else
-                {
+                {  //merr elementin qe ndodhet ne rreshtin e njejte por ne kolonen e karakterit tjeter
                     Console.Write(lista1[row1, col2] + "" + lista1[row2, col1]);
 
                 }
@@ -190,7 +199,7 @@ namespace Playfair
             }
         }
          //funksioni per dekriptim ku si parameter merret lista table dhe lista ciphertext
-        static void Dekriptimi(List<char> table, List<char> plainText)
+        static void Dekriptimi(List<char> table, List<char> ciphertext)
         {
             
             char[,] lista1 = new char[5, 5];//matrica 5x5
@@ -214,10 +223,10 @@ namespace Playfair
 
             Console.WriteLine();
             Console.Write("Teksti pas dekriptimit : ");
-            for (int i = 0; i < plainText.Count; i++)
+            for (int i = 0; i < ciphertext.Count; i++)
             {
-                char k1 = plainText[i]; //karakteri i pare
-                char k2 = plainText[i + 1];//karakteri i dyte
+                char k1 = ciphertext[i]; //karakteri i pare
+                char k2 = ciphertext[i + 1];//karakteri i dyte
                 int row1 = 0, row2 = -1, col1 = 0, col2 = -1;
 
                 for (int j = 0; j <25; j++)
