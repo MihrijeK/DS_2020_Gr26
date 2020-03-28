529c39967264ce65fc76cc1ce29e698b55bc5d98
LeonitaNika <58749225+LeonitaNika@users.noreply.github.com>
2020-03-28 23:47:43 +0100

    Update beale-cipher.cs

--- ds/beale-cipher.cs
+++ ds/beale-cipher.cs
@@ -9,27 +9,8 @@ namespace ds
 {
     class beale
     {
-        static void Main(string[] args)
-        {
-            if (args[0].Equals("e"))
-            {
-
-                string plaintext = args[2];
-                string libri = args[1];
-
-                Console.WriteLine(Enkriptimi(libri, plaintext));
-         
-            }
-if (args[0].Equals("d"))
-            {
-                var text = args[1];
-                string decrypt = args[2];
-
-                Dekriptimi(decrypt, text);
-            }
-        }
- //funksioni ne baze te cilit e enkriptojm mesazhin qe duam ta fshehim(plaintext) duke u bazuar ne nje liber(text).
- private static string Enkriptimi(string libri, string plaintext)
+        //funksioni ne baze te cilit e enkriptojm mesazhin qe duam ta fshehim(plaintext) duke u bazuar ne nje liber(text).
+         private static string Enkriptimi(string libri, string plaintext)
         {
             // per lexim  te file ( libri.txt)
             libri = File.ReadAllText(@"libri.txt");
@@ -40,10 +21,10 @@ if (args[0].Equals("d"))
             //me i kthy te gjitha shkronjat e plaintext ne shkronja te vogla
             plaintext = plaintext.ToLower();
             
-           // //me i nda secilen karakter te librit me hapsira
+            //me i nda secilen karakter te librit me hapsira
             string[] Dokument = libri.Split(' ');
             
-            ////  perdorimi i  metodes ToCharArray()  per kopjimin e karaktereve  nga string plaintext
+            //  perdorimi i  metodes ToCharArray()  per kopjimin e karaktereve  nga string plaintext
             //ne nje koleksion te Unicode character.
             char[] plain = plaintext.ToCharArray();
             string ch = "";
@@ -64,34 +45,34 @@ if (args[0].Equals("d"))
                 }     
                 //krijimi i nje for loop
                 // ku j merr vlerat deri tek gjatesia e Dokument
-                 for (int j = 0; j < Dokument.Length; j++)
+             for (int j = 0; j < Dokument.Length; j++)
+             {
+
+                 string str = Dokument[j];
+                //StartsWith percakton nese fillimi i stringut str pershtatet me te stringut plain[i]
+                if (str.StartsWith(plain[i].ToString()))
                 {
+                 //nese pershtatet atehere stringut strCh jepi vleren :
+                 strCh += j + 1 + " "; 
 
-                    string str = Dokument[j];
-                    //StartsWith percakton nese fillimi i stringut str pershtatet me te stringut plain[i]
-                    if (str.StartsWith(plain[i].ToString()))
-                    {
-                        //nese pershtatet atehere stringut strCh jepi vleren :
-                        strCh += j + 1 + " "; 
-
-                    }
                 }
-                //krijojm nje varg qe merr vleren e strch i ndare me ' '
-                string[] strArray = strCh.Split(' ');
+             }
+         //krijojm nje varg qe merr vleren e strch i ndare me ' '
+         string[] strArray = strCh.Split(' ');
                 
-                //k merr nje numer random deri te gjatesia e tekstit
-                int k = random.Next(strArray.Length - 1);
+         //k merr nje numer random deri te gjatesia e tekstit
+          int k = random.Next(strArray.Length - 1);
                 
-                //strArray merr indeksin k(random) dhe e kthen ne string
-                ch1 += strArray[k].ToString() + " ";
+          //strArray merr indeksin k(random) dhe e kthen ne string
+          ch1 += strArray[k].ToString() + " ";
                 
-                //ch e mbushim me vleren ch1 nga 0 deri te gjatesia ch1.Length-1
-                ch = ch1.Substring(0, ch1.Length - 1);
-            }
+          //ch e mbushim me vleren ch1 nga 0 deri te gjatesia ch1.Length-1
+          ch = ch1.Substring(0, ch1.Length - 1);
+      }
            
-            return ch;
-        }
-public static void Dekriptimi(string ciphertext, string tex)
+   return ch;
+}
+ public static void Dekriptimi(string ciphertext, string tex)
 {          // per lexim  te file ( libri.txt)
             tex = File.ReadAllText(@"libri.txt");
             //e ndajm chipertext me hapsira
@@ -102,7 +83,7 @@ public static void Dekriptimi(string ciphertext, string tex)
             string[] Dokument = tex.Split(' ');
  
 
-  for (int i = 0; i < t.Length; i++)
+   for (int i = 0; i < t.Length; i++)
             {    //Me ane te metodes Int32.Parse() e kemi  konvertu stringun ne integer 32bitesh per 
                  //me marr indeksin e fjaleve ne dokument.
                 st[i] = Dokument[(Int32.Parse(t[i])) - 1];
@@ -110,18 +91,11 @@ public static void Dekriptimi(string ciphertext, string tex)
             string cplain = "";
  
  
-  for (int l = 0; l < st.Length; l++)
+    for (int l = 0; l < st.Length; l++)
             {
-                
-                cplain += st[l].Substring(0, 1) + "";
+               cplain += st[l].Substring(0, 1) + "";
             }
             Console.Write(cplain);
-
-        }
-
-  }
-
-
-
-
+       }
+    }
 }
