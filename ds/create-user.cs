04af34927f3501bd26b441514eb8076fe4f21b2e
GresaSalihu <58749187+GresaSalihu@users.noreply.github.com>
2020-04-27 23:24:10 +0200

    Vendosja e gjithe celsave qe krijohen ne folderin keys

--- ds/create-user.cs
+++ ds/create-user.cs
@@ -1,1 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
 
+
+namespace createuser
+{
+    class createuser
+    {
+        static void Main(string[] args)
+        {
+            string Key_Name = args[0];
+            string Key_Path = "C://Users//dell//source//keys";
+            
