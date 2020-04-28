1a47009911c63425a3552a31fe0395232efdf626
GresaSalihu <58749187+GresaSalihu@users.noreply.github.com>
2020-04-28 23:29:24 +0200

    Importimi i librarive,krijimi i variablave te nevojshme

--- ds/delete-user.cs
+++ ds/delete-user.cs
@@ -1,1 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
 
+
+namespace deleteuser
+{
+    class deleteuser
+    {
+        static void Main(string[] args)
+        {
+            string KeyName = args[0];
+            string KeyPath = "C://Users//dell//source//keys";
+
+            string publik = String.Concat(KeyPath, "\\", KeyName, ".pub", ".xml");
+            string privat = String.Concat(KeyPath, "\\", KeyName, ".xml");
