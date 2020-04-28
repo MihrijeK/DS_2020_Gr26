d390bad2b01228e31b21182309642aa9a6286479
MihrijeK <42340737+MihrijeK@users.noreply.github.com>
2020-04-28 23:57:55 +0200

    Update write-message.cs

--- ds/write-message.cs
+++ ds/write-message.cs
@@ -26,7 +26,25 @@ namespace write_mesage
                 sw.Write(strXmlParameters);
                 sw.Close();
                 Console.WriteLine("Mesazhi i enkriptuar u rujt ne fajllin :" + "exportedKeys.xml");
-           
+            //Per emrin
+            byte[] name = Encoding.UTF8.GetBytes(args[1]);
+            //Ketu emrin po e kthejm ne base64
+            string part1 = Convert.ToBase64String(name);
+            //Per qelsin dhe IV
+            DESCryptoServiceProvider objDes = new DESCryptoServiceProvider(); //Des instanca objDES
+              //IV e gjeneruar random 8 bytes
+            objDes.GenerateIV();
+            string part2 = Convert.ToBase64String(objDes.IV);
+             //Qelesi i gjeneruar random 8 bytes
+            objDes.GenerateKey();
+            string keyStr = Convert.ToBase64String(objDes.Key);
+
+            string plaintexti=args[2];
+            string mesazhiiEnkriptuar=Encrypt(plaintexti,keyStr);
+            byte[] mesazhi=Encoding.UTF8.GetBytes(mesazhiiEnkriptuar);
+            string part3=Convert.ToBase64String(mesazhi);
+            Console.WriteLine(part1 + "." + part2+ "." + part3);
+            
 
             }
         }
@@ -38,5 +56,23 @@ namespace write_mesage
 
             return Convert.ToBase64String(byteCiphertexti);
         }
+        //Enkriptimi i Mesazhit me DES
+        public static string Encrypt(string plaintext, string key)
+        {
+            byte[] bytePlaintext = Encoding.UTF8.GetBytes(plaintext);
+            DESCryptoServiceProvider objDES = new DESCryptoServiceProvider();
+            if (key.Length > 8 || key.Length < 8)
+                return "Celesi i dhene nuk eshte adekuat !";
+            objDES.Key = Encoding.UTF8.GetBytes(key);
+            objDES.Padding = PaddingMode.None;
+            objDES.Mode = CipherMode.CBC;
+            //objDES.IV = Encoding.UTF8.GetBytes("11112222");
+            objDES.GenerateIV();
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, objDES.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(bytePlaintext, 0, bytePlaintext.Length);
+            cs.Close();
+            byte[] byteCiphertext = ms.ToArray();
+            return Convert.ToBase64String(byteCiphertext);
     }
 }
