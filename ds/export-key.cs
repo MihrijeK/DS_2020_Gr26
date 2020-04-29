cb2ca96f5423098d9277338011570a67b3189063
LeonitaNika <58749225+LeonitaNika@users.noreply.github.com>
2020-04-29 23:49:16 +0200

    Krijimi i argumenteve

--- ds/export-key.cs
+++ ds/export-key.cs
@@ -7,4 +7,14 @@ using System.Diagnostics;
 using System.Security.Cryptography;
 using System.IO;
 using System.Xml.Serialization;
-
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+        static void Main(string[] args)
+        {
+            string KeyName = args[0];
+            string pp = args[1];
+            string KeyPath = "C:\\Users\\Admin\\source\\repos\\lll\\prova\\bin\\Debug\\key";
+            string exportfolder = args[2];
