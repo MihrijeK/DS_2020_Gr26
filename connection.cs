f3e1636edf5325964cb0e02283f11fc92570f8cb
GresaSalihu <58749187+GresaSalihu@users.noreply.github.com>
2020-06-07 20:31:15 +0200

    Krijimi i lidhjes me databaze .

--- connection.cs
+++ connection.cs
@@ -10,7 +10,7 @@ namespace ds
         {
             try
             {
-                MySqlConnection conn = new MySqlConnection("server=localhost;database=users;userid=root;password=password123.");
+                MySqlConnection conn = new MySqlConnection("server=localhost;database=siguria;userid=root;password=password123.");
                 conn.Open();
 
                 MySqlCommand cmd = new MySqlCommand(sql, conn);
@@ -26,6 +26,21 @@ namespace ds
                 throw new Exception(exeption.Message);
             }
         }
-
+        public static DataSet DataSet(string sql)
+        {
+            MySqlConnection conn = new MySqlConnection("server=localhost;database=siguria;userid=root;password=password123.");
+            DataSet DS = new DataSet();
+            var SqlCommnad = new MySqlCommand(sql, conn);
+            var DA = new MySqlDataAdapter(SqlCommnad);
+            try
+            {
+                DA.Fill(DS);
+                return DS;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
