0d50625d98f10189d3e76029bfcb60d8afd665cb
GresaSalihu <58749187+GresaSalihu@users.noreply.github.com>
2020-06-07 22:42:19 +0200

    Komandat status,login dhe disa permirsime ,faza e trete .

--- ds.cs
+++ ds.cs
@@ -15,22 +15,25 @@ namespace ds
         static RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
         static void Main(string[] args)
         {
+            //gjenerimi i celesit 8byte  me ane te perdorimit RNGCryptoServiceProvider()
          RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
             byte[] key = new byte[8];
             rngCsp.GetBytes(key);
             
             byte[] encryptedData;
             byte[] decryptedData;
+            //nese nuk shenohet argumenti i pare
             if (args.Length == 0)
             {
                 Console.WriteLine("Shenoni njeren nga komandat :create-user,delete-user,export-key,import-key,login,status,beale,playfair,write-message,read-message,morse-code");
                 Environment.Exit(1);
             }
-
+            //validimi i argumentit te pare
             string argument = args[0].ToLower();
             TokenStatus A = new TokenStatus();
             
-            if (args[0].Equals("beale"))
+            //komanda Beale
+            if (argument == "beale")
             {
                   if (args.Length == 4)
                 {
@@ -40,6 +43,7 @@ namespace ds
                    {
                          string plaintext = args[3];
                          string libri = args[2];
+                         //plaintext duhet te permbaj vetem shkronja
                          if (Regex.Matches(plaintext, @"[a-zA-Z]").Count != 0)
                          {
                             Console.WriteLine(Beale.Enkriptimi(libri, plaintext));
@@ -55,6 +59,7 @@ namespace ds
                    {
                    var text = args[2];
                    string decrypt = args[3];
+                       //ciphertext duhet te permbaj vetem numra
                    if (Regex.Matches(decrypt, @"[0-9]").Count != 0)
                    {
                         Beale.Dekriptimi(decrypt, text);
@@ -71,7 +76,9 @@ namespace ds
                     Environment.Exit(1);
                    }
             }
-            if (args[0].Equals("Playfair"))
+            
+          //komanda playfair
+            else if (argument == "playfair")
             {
               Playfair playfair = new Playfair();
               if (args.Length == 4)
@@ -81,6 +88,7 @@ namespace ds
                     string plainText = args[3];
                     Playfair.initPlainText(plainText);
                     string qelesi = args[2];
+                    //plaintext dhe celsi duhet te permbajn vetem shkronja
                     if ((Regex.Matches(plainText, @"[a-zA-Z]").Count != 0) && (Regex.Matches(qelesi, @"[a-zA-Z]").Count != 0))
                   {
                     Playfair.Enkriptimi(Playfair.Krijotabelen(qelesi), Playfair.Krijoplaintekstiin());
@@ -95,7 +103,7 @@ namespace ds
                     string cipherText = args[3];
                     Playfair.initPlainText(cipherText);
                     string qelesi = args[2];
-
+              //ciphertext dhe celesi duhet te permbajn vetem shkronja
               if ((Regex.Matches(cipherText, @"[a-zA-Z]").Count != 0) && (Regex.Matches(qelesi, @"[a-zA-Z]").Count != 0))
               {
               Playfair.Dekriptimi(Playfair.Krijotabelen(qelesi), Playfair.Krijoplaintekstiin());
@@ -114,7 +122,8 @@ namespace ds
                     Environment.Exit(1);
                 }
             }
-            if (args[0].Equals("morse-code"))
+            //komanda morse-code
+            else if (argument == "morse-code")
             {
                if (args.Length == 3)
               {    
@@ -139,11 +148,12 @@ namespace ds
                    Environment.Exit(1);
               }
             }
-            if(args[0].Equals("export-key"))
+            //komanda export-key
+           else if (argument == "export-key")
             {
                
-                exportkey exporr = new exportkey();
-
+                exportkey exportt = new exportkey();
+                //nese vendoset edhe file se ku do te ruhet key
                 if (args.Length==4)
                 {
                     string KeyName1 = args[2];
@@ -152,6 +162,7 @@ namespace ds
                     exportkey.Eksporti(KeyName1, PP, exportfolder);
                    
                 }
+               //perndryshe shfaqe permbajtjen e celesit ne console
                 else if(args.Length==3)
                 {
                     string KeyName1 = args[2];
@@ -161,10 +172,12 @@ namespace ds
                 }
                 else
                 {
-                    Console.WriteLine("Shenoni vetem agrumentet e caktuara.");
+                    Console.WriteLine("Shenoni: export-key {hapesire} user {hapesire} public/private {hapesire} exportfolder apo export-key {hapesire} user {hapesire} public/private");
+                    Environment.Exit(1);
                 }
             }
-            if (args[0].Equals("import-key"))
+            //komanda import-key
+             else if (argument == "import-key")
             {
                 if (args.Length == 3)
                 {
@@ -172,7 +185,8 @@ namespace ds
                     {
                         string Keyname = args[1];
                         string shtegu = args[2];
-                        if (Regex.IsMatch(Keyname, "^[a-zA-Z0-9_]*$"))
+                        //emri duhet te permbaj vetem shkronja edhe _.
+                        if (Regex.IsMatch(Keyname, "^[a-zA-Z0-9_.]*$"))
                         {
                             import.Import(Keyname, shtegu);
                         }
@@ -187,13 +201,15 @@ namespace ds
                 }
                 else
                 {
-                    Console.WriteLine("Shenoni 3 argumente:import-user,Keyname dhe shtegun");
+                  Console.WriteLine("Shenoni :import-key {hapesire} user {hapesire} shtegu ");
+                  Environment.Exit(1);
                 }
 
             }
+            //komanda write-message
               else if (argument == "write-message")
             {
-
+                //nese vendosen vetem argumentet name dhe mesazhi
                 if (args.Length == 3)
                 {
 
@@ -233,8 +249,10 @@ namespace ds
                         Console.WriteLine("Gabim:Keni dhene komanda jo valide.");
                     }
                 }
+                 
                 else if (args.Length == 4)
                 {
+                    //nese si args[3] merret nje file ne te cilin duam ta shkruajm permbajtjen e mesazhit
                     if (args[3].Contains(".txt") || args[3].Contains(".xml"))
                     {
                         try
@@ -262,7 +280,7 @@ namespace ds
                             }
                             else
                             {
-                                Console.WriteLine("keq");
+                                Console.WriteLine("Komanda jo valide");
                             }
                         }
                         catch (CryptographicException e)
@@ -271,9 +289,114 @@ namespace ds
                             Environment.Exit(1);
                         }
                     }
+                    //perndryshe nese eshte shenuar nje token komanda write-message do ta kryej kete funksion
+                    else
+                    {
+                        try
+                        {
+                            string KeyName = args[1];
+                            byte[] Name = Encoding.UTF8.GetBytes(KeyName);
+                            string part1 = Convert.ToBase64String(Name);
+                            DESalg.GenerateIV();
+                            string part2 = Convert.ToBase64String(DESalg.IV);
+                            string KeyPath = "C://keys";
+
+                            byte[] Data = read_write.EnkriptimiIMesazhit(args[2], key, DESalg.IV);
+                            string mesazhi = Convert.ToBase64String(Data);
+
+                            string publik = String.Concat(KeyPath, "//", KeyName, ".pub", ".xml");
+                            string statusi = A.statusi(args[3]);
+                            encryptedData = read_write.RSAEncrypt(key, publik);
+                            string qelsi = Convert.ToBase64String(encryptedData);
+                            read_write obj = new read_write();
+                            ;
+                            if (!File.Exists(publik))
+                            {
+                                Console.WriteLine("Gabim: Celesi publik " + String.Concat(args[1]) + " nuk ekziston ");
+                            }
+                            else {
+                                
+                                string privat = String.Concat(KeyPath, "//", statusi, ".xml");
+
+                             //nese tokeni eshte valid 
+                            if ((A.Exp(args[3]) > DateTimeOffset.Now) && (A.VerifikoTokenin(args[3], A.statusi(args[3]), out string errorMessage)))
+                                {
+                                    byte[] derguesi = Encoding.UTF8.GetBytes(statusi);
+                                    string part4 = Convert.ToBase64String(derguesi);
+                                    byte[] signedData = read_write.Nenshkrimi(Data, privat);
+                                    string part5 = Convert.ToBase64String(signedData);
+
+                                    Console.WriteLine(part1 + "." + part2 + "." + qelsi + "." + mesazhi + "." + part4 + "." + part5);
+                                }
+
+                                else
+                                {
+                                    Console.WriteLine("Komanda ka deshtuar sepse tokeni nuk eshte valid !");
+                                    Environment.Exit(1);
+                                }
+                            }
+                            
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Gabim:keni dhene komanda jo valide . ");
+                            Environment.Exit(1);
+                        }
+                    }
+                }
+                  //nese shenojm name,mesazhin,tokenin dhe file ku duam ta ruajm mesazhin
+                else if (args.Length == 5)
+                {
+                    
+                    string KeyName = args[1];
+                    byte[] Name = Encoding.UTF8.GetBytes(KeyName);
+                    string part1 = Convert.ToBase64String(Name);
+                    DESalg.GenerateIV();
+                    string part2 = Convert.ToBase64String(DESalg.IV);
+                    string KeyPath = "C://keys";
+
+                    byte[] Data = read_write.EnkriptimiIMesazhit(args[2], key, DESalg.IV);
+                    string mesazhi = Convert.ToBase64String(Data);
+
+                    string statusi = A.statusi(args[3]);
+                    string privat = String.Concat(KeyPath, "//", statusi, ".xml");
+                    try
+                    {
+                        DESalg.GenerateIV();
+                       
+                        //nese tokeni eshte valid
+                        if ((A.Exp(args[3]) > DateTimeOffset.Now) && (A.VerifikoTokenin(args[3], statusi, out string errorMessage)))
+                        {
+                            string publik = String.Concat(KeyPath, "//", KeyName, ".pub", ".xml");
+                            encryptedData = read_write.RSAEncrypt(key, publik);
+                            string qelsi = Convert.ToBase64String(encryptedData);
+                            byte[] derguesi = Encoding.UTF8.GetBytes(statusi);
+                            string part4 = Convert.ToBase64String(derguesi);
+                            byte[] signedData = read_write.Nenshkrimi(Data, privat);
+                            string part5 = Convert.ToBase64String(signedData);
+                            string ciphertexti = part1 + "." + part2 + "." + qelsi + "." + mesazhi + "." + part4 + "." + part5;
+                            StreamWriter sw = new StreamWriter(args[4]);
+                            sw.Write(ciphertexti);
+                            sw.Close();
+                        }
+                        Console.WriteLine("Mesazhi i enkriptuar u ruajt ne fajllin " + args[4]);
+                    }
+                    catch (CryptographicException e)
+                    {
+                        Console.WriteLine("A Cryptographic error occurred: {0}", e.Message);
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Shenoni : write-message {hapesire} marresi {hapesire} mesazhi apo write-message {hapesire} marresi {hapesire} mesazhi {hapesire} token ");
+                    Environment.Exit(1);
+                }
+            }
+            //komanda read-message
             else if (argument == "read-message")
             {
-
+               //nese vendosim si argument  name dhe mesazhin
                 if (args.Length == 2)
                 {
 
@@ -289,14 +412,15 @@ namespace ds
                         String[] hyrja = strXmlParameters.Split('.');
                         try
                         {
-
+                           //Kqë nëse figuron pjesa e dërguesit/nënshkrimit në mesazh,
+                           //atëherë do të tentohet verifikimi i atij nënshkrimi duke përdorur çelësin publik të dërguesit. 
                             if (hyrja.Length == 6)
                             {
                                 string KeyPath = "C://keys";
                                 string privati = String.Concat(KeyPath, "\\", new ASCIIEncoding().GetString(Convert.FromBase64String(hyrja[0])), ".xml");
                                 decryptedData = read_write.RSADecrypt(Convert.FromBase64String(hyrja[2]), privati);
                                 string publiku = String.Concat(KeyPath, "\\", new ASCIIEncoding().GetString(Convert.FromBase64String(hyrja[4])), ".pub", ".xml");
-                                if (read_write.VerifySignedHash(Convert.FromBase64String(hyrja[3]), Convert.FromBase64String(hyrja[5]), publiku) == true)
+                                if (read_write.VerifikimiiNenshkrimit(Convert.FromBase64String(hyrja[3]), Convert.FromBase64String(hyrja[5]), publiku) == true)
                                 {
                                     Console.WriteLine("Marresi: " + new ASCIIEncoding().GetString(Convert.FromBase64String(hyrja[0])));
                                     Console.WriteLine("Mesazhi: " + read_write.DekriptimiIMesazhit(Convert.FromBase64String(hyrja[3]), decryptedData, Convert.FromBase64String(hyrja[1])));
@@ -313,6 +437,8 @@ namespace ds
                             }
                             else
                             {
+                                //Nëse mungon pjesa e dërguesit/nënshkrimit, atëherë komanda e injoron dhe vepron sikur në fazën e
+                                //dytë. 
                                 string strXmlParameeters = "";
                                 StreamReader ssr = new StreamReader(args[1]);
                                 strXmlParameeters = ssr.ReadToEnd();
@@ -341,7 +467,7 @@ namespace ds
 
                     }
 
-
+                  
                     else
                     {
                         String[] hyrja = args[1].Split('.');
@@ -355,7 +481,7 @@ namespace ds
                             if (hyrja.Length == 6)
                             {
                                 string publiku = String.Concat(KeyPath, "\\", new ASCIIEncoding().GetString(Convert.FromBase64String(hyrja[4])), ".pub", ".xml");
-                                if (read_write.VerifySignedHash(Convert.FromBase64String(hyrja[3]), Convert.FromBase64String(hyrja[5]), publiku) == true)
+                                if (read_write.VerifikimiiNenshkrimit(Convert.FromBase64String(hyrja[3]), Convert.FromBase64String(hyrja[5]), publiku) == true)
                                 {
                                     Console.WriteLine("Marresi: " + new ASCIIEncoding().GetString(Convert.FromBase64String(hyrja[0])));
                                     Console.WriteLine("Mesazhi: " + read_write.DekriptimiIMesazhit(Convert.FromBase64String(hyrja[3]), decryptedData, Convert.FromBase64String(hyrja[1])));
@@ -402,7 +528,8 @@ namespace ds
                     Console.WriteLine("Sheno: read-message {hapesire} file/mesazh");
                 }
             }
-            if (args[0].Equals("create-user"))
+            //komanda create-user
+             else if (argument == "create-user")
             {
                 if (args.Length == 2)
                 {
@@ -424,15 +551,73 @@ namespace ds
                 
                 else
                 {
-                    Console.WriteLine("Shenoni 2 argumente:create-user dhe KeyName");
+                    Console.WriteLine("Shenoni : create-user {hapesire} KeyName");
                     Environment.Exit(1);
                 }
 
             }
-            else if (args[0].Equals("delete-user"))
+            //komanda delete-user
+             else if (argument == "delete-user")
             {
+                if (args.Length == 2)
+                {
+                    Deleteuser.Largo(args[1]);
+                }
+                else
+                {
+                    Console.WriteLine("Shenoni: delete-user {hapesire} user");
+                    Environment.Exit(1);
+                }
+            }
+            //komanda login
+            else if (argument == "login")
+            {
+                if (args.Length == 2)
+                {
+                    try
+                    {
+                        login loginn = new login();
+                        string username = args[1];
+                        loginn.Login(username);
+                    }
 
-                Deleteuser.Largo(args[1]);
+                    catch (Exception error)
+                    {
+                        throw new Exception(error.Message);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Shenoni:create-user {hapesire} user");
+                    Environment.Exit(1);
+                }
+            }
+            //komanda status
+            else if (argument == "status")
+            {
+                if (args.Length == 2)
+                {
+                    bool status = A.status(args[1]);
+
+                    if (status)
+                    {
+                        Console.WriteLine("Valid: po");
+                    }
+                    else if (!status)
+                    {
+                        Console.WriteLine("Valid: jo");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Shenoni status {hapesire} token");
+                }
+            }
+            else
+            {
+               Console.WriteLine("Shenoni njeren nga komandat :create-user,delete-user,export-key,import-key,login,status,beale,playfair,write-message,read-message,morse-code");
+               Environment.Exit(1);
+            
             }
         }
     }
